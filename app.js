15dcb6d93537d60fdad507b678d2c455ca5d1bad
samskarvan <samskarvan@gmail.com>
2018-05-10 17:39:18 -0700

    added bundle file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const cors =require('cors');
 const PORT = process.env.PORT || 9000;
-
+const {resolve} = require('path');
 const app=express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
-app.get('/user-data',(req,res)=>{
+
+app.get('/api/user-data',(req,res)=>{
     const user={
         name: 'Holden Caulfield',
         email: 'ih8phonies@penceyprep.edu'
@@ -15,10 +18,31 @@ app.get('/user-data',(req,res)=>{
     res.send(user);
 });
 
-//create an endpoint for testing (the '/' is the endpoint)
-app.get('/', (req, res)=>{
-    res.send('<h1>Algo Completamente Diferente</h1>');
+app.get('/api/get-article', (req, res)=>{
+    const article ={
+        title: 'Los Mejores Restaurantes Que Se Ofrecen Mimosas Gratises Cerca de Irvine, CA',
+        author: 'Los Muchachos Amables de LFZ',
+        content: 'mimosas gratises son los mejores bebidos del mundo.'
+    }
+
+    res.send(article);
 });
+
+
+app.post('/api/send-data',(req,res)=>{
+    console.log('data',req.body);
+
+    res.send({
+        success: true,
+        boomerang: req.body
+    });
+})
+
+//create an endpoint for testing (the '/' is the endpoint)
+app.get('*', (req, res)=>{
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on localhost:', PORT);
--- client/src/components/test.js
+++ client/src/components/test.js
@@ -4,12 +4,29 @@ import axios from 'axios';
 class Test extends Component{
     componentDidMount(){
         this.getUserData();
+        this.getArticle();
+        this.sendData();
     }
     async getUserData(){
-        const result = await axios.get('http://localhost:9000/user-data');
+        const result = await axios.get('/api/user-data');
 
         console.log('user result:', result);
     
+    }
+    async getArticle(){
+        const article = await axios.get('/api/get-article');
+
+        console.log('article result:', article);
+    
+    }
+    async sendData(){
+        const dataToSend={
+            something: 'this is something',
+            another: 'this is another one'
+        };
+        const response = await axios.post('/api/send-data', dataToSend);
+
+        console.log('send data:', response);
     }
     render(){
         return <h1>Feliz Juevessss</h1>
